a66c62dc20db89259af11cca861d230677a85548
Sunaina1006 <sunainabhola2010@gmail.com>
2024-01-09 19:35:41 -0800

    Initial Commit

---
+++ MyfirstApp/MyfirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyfirstAppApp.swift
+//  MyfirstApp
+//
+//  Created by user236639 on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MyfirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
